51ca18f44d5d4d1f8f1a2cf7035d5a8c1d0b9d20
Matías <matias.abarca2018@gmail.com>
2025-03-18 18:44:25 -0300

    Se termina endpoints básicos de RoleController y algunas modificaciones en service para obtener el name del role el lowerCase y guardar en lowerCase

--- src/main/java/com/matias/projects/profiles/controllers/RoleController.java
+++ src/main/java/com/matias/projects/profiles/controllers/RoleController.java
@@ -2,8 +2,11 @@ package com.matias.projects.profiles.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,4 +31,21 @@ public class RoleController {
     public List<Role> getAllRoles() {
         return roleService.getAllRoles();
     }
+    @GetMapping("{id}")
+    public Role getRoleById(@PathVariable Long id) {
+        return roleService.getRoleById(id);
+    }
+    @GetMapping("/name/{name}")
+    public Role getRoleByName(@PathVariable String name) {
+        return roleService.getRoleByName(name.toLowerCase());
+    }
+    @PutMapping
+    public Role updateRole(@RequestBody Role role) {
+        return roleService.updateRole(role);
+    }
+    @DeleteMapping("{id}")
+    public void deleteRole(@PathVariable Long id) {
+        roleService.deleteRole(id);
+    }
+
 }
