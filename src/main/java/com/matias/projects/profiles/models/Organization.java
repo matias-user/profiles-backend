c955586583b6bf2fe7de7b45b8d0018a01674ecc
Matías <matias.abarca2018@gmail.com>
2025-03-24 16:10:51 -0300

    Creando y relacionado la entidad Organización a usuarios

---
+++ src/main/java/com/matias/projects/profiles/models/Organization.java
@@ -0,0 +1,31 @@
+package com.matias.projects.profiles.models;
+
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Organization {
+    
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private Long id;
+    private String name;
+    private Integer phoneNumber;
+    private String email;
+    private String address;
+    private String city;
+
+    @OneToMany( mappedBy = "organization" )
+    private Set<User> users;
+}
--- src/main/java/com/matias/projects/profiles/models/User.java
+++ src/main/java/com/matias/projects/profiles/models/User.java
@@ -2,6 +2,8 @@ package com.matias.projects.profiles.models;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -10,6 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -34,5 +37,9 @@ public class User {
         inverseJoinColumns = @JoinColumn( name = "role_id" )
     )
     private List<Role> roles;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "organization_id")
+    @JsonIgnore 
+    private Organization organization;
 
 }
